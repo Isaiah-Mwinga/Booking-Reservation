3f73edd4c780ed455e5c8729c753a42331a42778
@Brandon Mwinga <imotongori@kabarak.ac.ke>
2022-04-28 11:33:52 +0300

    going through the useState hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React, {useState} from 'react';
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-     
-      </header>
+     <p>You clicked {count} times</p>
+     <button onClick={()=> setCount(count + 1)}>
+       Click me
+     </button>
     </div>
   );
 }
