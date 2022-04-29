4658c5f5930602a92aa41837a5a196ef1a23ad85
@Brandon Mwinga <imotongori@kabarak.ac.ke>
2022-04-29 08:39:14 +0300

    Added bootsrap for styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, {useState} from 'react';
+import '../App.css';
+import Navbar from './components/Navbar';
+
+
 function App() {
-  const [count, setCount] = useState(0);
+  
   return (
-    <div className="App">
-     <p>You clicked {count} times</p>
-     <button onClick={()=> setCount(count + 1)}>
-       Click me
-     </button>
-    </div>
+    <>
+    <Navbar />
+    </>
   );
 }
 
