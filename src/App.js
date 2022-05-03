06d62f9e397daf9a7e807999108bd91495a93edb
@Brandon Mwinga <imotongori@kabarak.ac.ke>
2022-05-03 09:11:03 +0300

    added components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import '../App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import Navbar from './components/Navbar';
+import Home from "./pages/Home/Home";
+import { List } from "./pages/List/List";
 
 
 function App() {
   
   return (
-    <>
-    <Navbar />
-    </>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/hotels" element={<List/>}/>
+  </Routes>
+  </BrowserRouter>
   );
 }
 
