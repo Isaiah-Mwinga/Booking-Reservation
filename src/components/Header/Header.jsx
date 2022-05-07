39b642bf0e0a68c4660f398837242ba287f715f3
@Brandon Mwinga <imotongori@kabarak.ac.ke>
2022-05-07 03:42:47 +0300

    updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import "./header.css"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header ">
+      <div className="headerList">
+      </div>
+    </div>
   )
 }
 
